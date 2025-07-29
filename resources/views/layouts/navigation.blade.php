0c7939dabe43387a23caf579538eb4f38554b54e
muhammedkusay <mhmadhamado35@gmail.com>
2025-07-29 18:33:26 +0300

    resolved some issues in dark mode

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,16 +18,7 @@
                 </div>
             </div>
             {{-- theme --}}
-            <button id="theme-toggle" class="relative h-7 p-1.5 ml-auto mr-3 md:mr-0 my-auto text-sm flex gap-1 items-center cursor-pointer rounded-full border shadow-inner overflow-hidden bg-white dark:bg-neutral-800 dark:border-neutral-700">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-neutral-600 dark:text-neutral-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
-                </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4 text-neutral-600 dark:text-neutral-400">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
-                </svg>
-                <div id="theme-inner-toggle" class="absolute size-5 left-0 bg-gray-300 rounded-full shadow-md dark:bg-neutral-600 transition-all duration-300"></div>
-            </button>
-
+            <x-theme-button style="my-auto ml-auto mr-3 md:mr-0"/>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
