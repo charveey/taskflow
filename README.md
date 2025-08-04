fdf044fb84d8f6b2c629a98e5ef1519b07f1d069
muhammedkusay <mhmadhamado35@gmail.com>
2025-08-04 10:21:13 +0300

    Created readme file for installation guide

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # Taskflow
 
 A project management system created using Laravel + postgres
+
+# Install the project to your enviroment
+This project is using breeze starter kit
+
+```
+composer require laravel/breeze --dev
+```
+
+Then migrate the database
+
+```
+php artisan migrate
+```
+
+Install node packages and run
+
+```
+npm install
+```
+
+```
+npm run dev
+```
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -4,8 +4,49 @@
         {{__('Projects')}}
     </x-slot>
 
-    <div class="p-3 md:p-4 dark:text-neutral-50">
-        
+    <div class="p-3 md:p-4 grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 dark:text-neutral-50">
+
+        {{-- projects --}}
+        <a href="{{ route('projects.index') }}" class="block h-full rounded-2xl overflow-hidden shadow-sm">
+            {{-- title --}}
+            <div class="h-20 flex items-center justify-center bg-indigo-200 dark:bg-indigo-900">
+                <p class="text-lg md:text-xl">Php project for this one</p>
+            </div>
+            <div class="p-3 flex flex-col gap-4 bg-white dark:bg-neutral-900">
+                {{-- description --}}
+                <p class="text-neutral-800 dark:text-neutral-100">Description:Php project for this one Php project for this one</p>
+                {{-- progress bar --}}
+                <div>
+                    <div class="flex justify-between mb-1">
+                        <span class="text-sm font-medium text-blue-700 dark:text-white">Completed Tasks</span>
+                        <span class="text-sm font-medium text-blue-700 dark:text-white">45%</span>
+                    </div>
+                    <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                        <div class="bg-blue-600 h-2.5 rounded-full" style="width: 45%"></div>
+                    </div>
+                </div>
+                {{-- start date & users --}}
+                <div class="flex items-center justify-between gap-2 text-sm text-neutral-600 dark:text-neutral-400">
+                    {{-- start date --}}
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                        </svg>
+                        <p>03/08/2025</p>
+                    </div>
+                    {{-- users count --}}
+                    <div class="flex items-center gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                        </svg>
+                        <p>4</p>
+                    </div>
+                </div>
+                
+            </div>
+        </a>
+
+        {{-- create project --}}
         <x-create-project />
 
     </div>
